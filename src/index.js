89a793a0398f6d8f8f0684236944c1d37d731122
bBob21 <102711034+bBob21@users.noreply.github.com>
2025-07-08 13:05:35 +1000

    Added prettier and elint

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import "./styles.css";
-import {Todo} from "./todo.js";
-import {Project} from "./project.js";
-import {createEventSidebar, render} from "./render.js";
+import { Todo } from "./todo.js";
+import { Project } from "./project.js";
+import { createEventSidebar, render } from "./render.js";
 
 const projectList = [];
 window.projectList = projectList;
+
 const todo1 = new Todo("todoTitle", "01/01/2026");
 const project1 = new Project("ProJect_Name");
 project1.addTodo(todo1);
-const project2 = new Project("name2pRoject2")
+const project2 = new Project("name2pRoject2");
 
 projectList.push(project1, project2);
 createEventSidebar(projectList);
 render(projectList);
-
